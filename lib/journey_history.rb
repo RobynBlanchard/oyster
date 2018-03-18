38959d8c83a67b7c376b3cd24cd8540aebc771cd
Robyn <Robyn@nothsadmins-MacBook-Pro.local>
2018-03-18 18:53:15 +0000

    Update gems

---
+++ lib/journey_history.rb
@@ -0,0 +1,13 @@
+class JourneyHistory
+  PENALTY_FARE = 5
+
+  attr_accessor :journeys
+
+  def initialize(journeys=[])
+    @journeys = journeys
+  end
+
+  def add(journey)
+    journeys << journey
+  end
+end
